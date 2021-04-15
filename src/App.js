ea125f2c2f066581ec5826973fd0964d222d3c2c
scrungus <scrungus@riseup.net>
2021-04-15 20:10:00 +0100

    changed theme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+import NavBar from './components/navbar';
+import Home from './pages/home'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light:"#464557",
+      main: "#1e1e2e",
+      dark:"#000004",
+      contrastText:"#ffffff"
+    },
+    secondary: {
+      light:"#ffbba2",
+      main: "#ff8a73",
+      dark:"#c85a47",
+      contrastText:"#000000"
+    }
+  }
+});
+
+class App extends Component {
+  render() {
+    
+    return (
+      <ThemeProvider theme={theme}>
+         <div>
+          <NavBar></NavBar>
+        </div>
+        <Home></Home>
+      </ThemeProvider>   
+    );
+  }
 }
-
 export default App;
